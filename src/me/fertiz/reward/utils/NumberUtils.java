cdb98d7f539bbdacf1e604876ec9aed46a93559c
DX_IT <72042711+bxlachaxdd@users.noreply.github.com>
2021-06-13 16:57:44 +0200

    Update NumberUtils.java

--- src/me/fertiz/reward/utils/NumberUtils.java
+++ src/me/fertiz/reward/utils/NumberUtils.java
@@ -1,6 +1,8 @@
 package me.fertiz.reward.utils;
 
 public class NumberUtils {
+    
+    //say whaaaaaaaaaaaaaaaaaaaaaaaat?!!?!?
 
     public static boolean isNumber(String text) {
         return text.chars().allMatch(Character::isDigit);
