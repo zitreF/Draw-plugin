b08084416fa234165ba52b3133eecefb381c43ff
DX_IT <72042711+bxlachaxdd@users.noreply.github.com>
2021-06-13 16:53:20 +0200

    Update ChatUtil.java

--- src/me/fertiz/reward/utils/ChatUtil.java
+++ src/me/fertiz/reward/utils/ChatUtil.java
@@ -3,17 +3,22 @@ package me.fertiz.reward.utils;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
-public class ChatUtil {
+public final class ChatUtil {
+    
+    //co to za guwno Xddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+    
+    private ChatUtil() {
+    }
 
     public static String fixColor(String text) {
         return ChatColor.translateAlternateColorCodes('&', text).replace(">>", "»").replace("<<", "«");
     }
 
     public static void sendMessage(Player p, String text) {
-        p.sendMessage(fixColor(text));
+        p.sendMessage(fixColor(text)); // ??????????????????????????????????????????
     }
 
     public static void sendTitle(Player p, String subtitle) {
-        p.sendTitle(fixColor("&8✰ &6NAGRODA &8✰"), fixColor(subtitle));
+        p.sendTitle(fixColor("&8✰ &6NAGRODA &8✰"), fixColor(subtitle)); //co
     }
 }
